f8021187eb675aa039549ec45e5c5120ed12588d
NickSwag <1282622481@qq.com>
2019-04-19 19:08:21 +0800

    0419-12.8.7-8重构-创建update_bullets()和fire_bullets(),精简主程序代码

--- game_functions.py
+++ game_functions.py
@@ -15,10 +15,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         # 向左移动飞船
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # 创建一个子弹并将其加入到编组bullets中
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullets(ai_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
@@ -49,3 +46,20 @@ def update_screen(ai_settings, screen, ship, bullets):
 
     # 让最近绘制的屏幕可见
     pygame.display.flip()
+
+def update_bullets(bullets):
+    """更新子弹的位置，并删除已消失的子弹"""
+    # 更新子弹的位置
+    bullets.update()
+
+    # 删除已消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def fire_bullets(ai_settings, screen, ship, bullets):
+    """如果还没有达到限制，就发射一颗子弹"""
+    # 创建一个子弹并将其加入到编组bullets中
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
