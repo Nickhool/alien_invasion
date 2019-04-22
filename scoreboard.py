ed9c2e443c3200ae69a56416daff23555a6ab5de
NickSwag <1282622481@qq.com>
2019-04-22 16:58:27 +0800

    0422-14.3.7 最高得分

--- scoreboard.py
+++ scoreboard.py
@@ -18,6 +18,7 @@ class Scoreboard():
 
         # 准备初始化得分图像
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """将得分转换为一幅渲染的图像"""
@@ -27,10 +28,24 @@ class Scoreboard():
                                             self.ai_settings.bg_color)
 
         # 将得分放在屏幕右上角
-        self.rect = self.score_image.get_rect()
-        self.rect.right = self.screen_rect.right - 20
-        self.rect.top = 20
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def prep_high_score(self):
+        """将最高分转换为渲染的图片"""
+        high_score = int(round(self.stats.high_score, -1))
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color,
+                                                 self.ai_settings.bg_color)
+
+        # 将最高分放在屏幕顶部中央
+        self.high_score_rect =  self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = 20
+
 
     def show_score(self):
         """在屏幕上显示得分"""
-        self.screen.blit(self.score_image, self.rect)
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
