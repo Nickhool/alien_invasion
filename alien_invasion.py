361deb4f05d1d69987a237e88adcc0732055a8fe
NickSwag <1282622481@qq.com>
2019-04-22 14:59:04 +0800

    0422-14.3.1-2 显示得分 创建记分牌

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ import pygame
 from  pygame.sprite import Group
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 import game_functions as gf
@@ -20,8 +21,9 @@ def run_game():
 
     # 创建play按钮
     play_button = Button(ai_settings, screen, 'Play')
-    # 创建一个用于存储游戏统计信息的实例
+    # 创建一个用于存储游戏统计信息的实例, 并创建记分牌
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
     # 创建一艘飞船 一个子弹编组和一个外星人编组
     ship = Ship(ai_settings,screen)
     bullets = Group()
@@ -36,6 +38,6 @@ def run_game():
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
+        gf.update_screen(ai_settings,screen,stats,sb,ship,aliens,bullets,play_button)
 
 run_game()
